78ac8058ab57b53733aa93e310a4eee3217a798a
Thales Gelinger <thalesgelinger@Thaless-MacBook-Air.local>
2025-03-13 17:46:53 -0300

    feat: add base admin modal to use

--- src/app/(tabs)/admin.tsx
+++ src/app/(tabs)/admin.tsx
@@ -1,18 +1,88 @@
-import { Text, View, XStack } from "tamagui";
-import { Check } from "lucide-react-native"
+import { Button as TButton, Image, Sheet, Text, useTheme, View, XStack, YStack } from "tamagui";
+import { Check, Edit, FileCheck, Truck, User } from "lucide-react-native"
+
+import React, { useState } from 'react'
+import { FlatList } from "react-native";
+import { LoadTile } from "@/src/shared/ui/load-tile";
+import { LoadType } from "@/src/types/load";
+import { Button } from "@/src/shared/ui/button";
 
 export default function Admin() {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const theme = useTheme()
+
+    const loads: Array<LoadType> = [
+        {
+            client: "Fulano 1",
+            plate: "ABC-1234",
+            quantity: 40,
+            material: "areia",
+            signatureUrl: "https://scontent.fpoa2-1.fna.fbcdn.net/v/t39.30808-6/296375877_343371377999777_4026508833316169967_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=FaHf1tp7TgEQ7kNvgHbG2U1&_nc_oc=AdjjJtEp7Rfy4iBlwXrjd9TSdsfztD8b76GJUCpMo7ymva8RznLjI52SUGRJMRSiTm0&_nc_zt=23&_nc_ht=scontent.fpoa2-1.fna&_nc_gid=APnnDuuEtVGd-VwCXS-KIXk&oh=00_AYE7caeWN6asnSslNmUirUyHSvU_VB_ZLRzhqP0jhmuBTg&oe=67D91151",
+            date: new Date()
+        }
+    ]
+
     return (
-        <View
-            flex={1}
-            justifyContent="center"
-            alignItems="center"
-        >
-            <XStack alignItems="center">
-                <Check color="red" size={48} />
-                <Text>Admin Screen</Text>
-            </XStack>
-        </View>
+        <>
+            <View padding={20} backgroundColor={theme.background?.val} flex={1} position="relative">
+                <FlatList
+                    contentContainerStyle={{ flexGrow: 1 }}
+                    data={loads}
+                    renderItem={({ item }) => <LoadTile
+                        client={item.client}
+                        plate={item.plate}
+                        quantity={item.quantity}
+                        material={item.material}
+                        signatureUrl={item.signatureUrl}
+                        date={item.date}
+                    />}
+                    ItemSeparatorComponent={() => <View height={12} />}
+                    keyExtractor={(_, i) => i.toString()}
+                    ListEmptyComponent={() => (
+                        <YStack
+                            flex={1}
+                            alignItems="center"
+                            justifyContent="center"
+                            gap={12}
+                        >
+                            <Text fontSize={24}>Tudo certo, todos os carregamentos foram enviados</Text>
+                            <FileCheck color={theme.main?.val} size={72} />
+                        </YStack>
+                    )}
+                />
+
+                <TButton
+                    backgroundColor={theme.main?.val}
+                    height={48}
+                    width={48}
+                    position="absolute"
+                    bottom={10}
+                    right={10}
+                    onPress={() => setIsOpen(true)}
+                >
+                    <Edit color={theme.strongGrey?.val} size={24} />
+                </TButton>
+            </View>
+
+            <Sheet
+                modal
+                open={isOpen}
+                onOpenChange={setIsOpen}
+                snapPoints={[70]}
+                dismissOnSnapToBottom
+            >
+                <Sheet.Overlay backgroundColor="'rgba(0, 0, 0, 0.3)'" />
+                <Sheet.Frame padding="20" >
+                    <YStack gap="$5">
+                        <Button label="Clientes" Icon={User} color={theme.main?.val} />
+                        <Button label="Materiais" Icon={Truck} color={theme.main?.val} />
+
+                    </YStack>
+                </Sheet.Frame>
+            </Sheet>
+        </>
     );
 }
 
